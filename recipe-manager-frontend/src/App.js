4a2ac5fb9845b2e8fa27af93fab67c6bd6352600
Dhananjay Talati <dhananjaytalati@Dhananjays-MacBook-Pro.local>
2025-01-25 05:33:23 +0530

    Day 1 Push: Added buttons with images dynamically from react and made a grid on UI

--- recipe-manager-frontend/src/App.js
+++ recipe-manager-frontend/src/App.js
@@ -1,2 +1,39 @@
 import { use, useState } from "react";
 import ToggleSwitch from "./components/ToggleSwitch/ToggleSwitch"
+import ScrollableComponent from "./components/ScrollableComponent/ScrollableComponent"
+import "./App.css";
+
+const RecipeButton = ({ images}) => {
+    return (
+    <div className="recipe-grid">
+        {images.map((image_name, index) => (
+            <button
+                key={index}
+                className="Big-button"
+                style={{
+                    backgroundImage: `url(/static/${image_name})`,
+                }}>
+            </button>
+        ))}
+    </div>
+    );
+  };
+// Example usage:
+
+const imagePaths = [
+    "spagethi_phone.webp",
+    "pizza.jpeg",
+    "Roasted-Tomato-and-Olive-Focaccia-Bread-2.jpg",
+    "sourdough-bread-round-1-of-1.webp",
+    "spaghetti-aglio-e-olio-close-up-800x1200.jpg",
+    "Chocolate-Brownie-Recipe-5.jpg",
+    'chole-masala-recipe27.jpg',
+    'sev-tameta-nu-shaak.jpg',
+    'Perfect-Cinnamon-Rolls-10-1067x1600.jpg'
+  ];
+
+export default function Home(){
+    return <RecipeButton
+        images={imagePaths}/>;
+}
+
---
+++ recipe-manager-frontend/src/components/ScrollableComponent/ScrollableComponent.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+function ScrollableComponent() {
+
+  return (
+
+    <div style={{ height: '200px', overflow: 'scroll' }}>
+
+      {/* Your content here */}
+
+    </div>
+
+  );
+
+}
+
+
+export default ScrollableComponent;
